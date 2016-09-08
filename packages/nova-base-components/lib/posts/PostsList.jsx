3a9fb253abe04d1253c507ae8a382f9b7280391a
trujunzhang <trujunzhang@gmail.com>
2016-09-08 16:13:46 +0800

    newsletter.

--- packages/nova-base-components/lib/posts/PostsList.jsx
+++ packages/nova-base-components/lib/posts/PostsList.jsx
@@ -13,6 +13,28 @@ const PostsList = ({results, currentUser, hasMore, ready, count, totalCount, loa
     return (
       <div className="content_1jnXo">
           <section className="results_37tfm">
+              {/*render left panel*/}
+              <div>
+                  <div className="fullWidthBox_3Dggh box_c4OJj">
+                      <div className="content_DcBqe">
+                          <Telescope.components.PostsListTitle/>
+                          <div className="posts_275PF">
+                              <ul className="postsList_2tOc7">
+                                  {results.map(post =>
+                                    <li>
+                                        <Telescope.components.PostsItem post={post}
+                                                                        currentUser={currentUser}
+                                                                        key={post._id}/>
+                                    </li>
+                                  )}
+                              </ul>
+                          </div>
+                          {hasMore ? (ready ? <Telescope.components.PostsLoadMore loadMore={loadMore} count={count}
+                                                                                  totalCount={totalCount}/> :
+                            <Telescope.components.PostsLoading/>) : null}
+                      </div>
+                  </div>
+              </div>
 
           </section>
           <div className="sidebar_Y2LGQ">
