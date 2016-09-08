fb3d64f78ef46c0c58ba43920b369a6f7f82cdde
trujunzhang <trujunzhang@gmail.com>
2016-09-08 14:58:43 +0800

    message.

--- packages/nova-base-components/lib/posts/PostsLoadMore.jsx
+++ packages/nova-base-components/lib/posts/PostsLoadMore.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
-var Waypoint = require('react-waypoint');
+//var Waypoint = require('react-waypoint');
 
 const PostsLoadMore = ({loadMore, count, totalCount}) => {
     return (
       <div>
-          <Waypoint
-            onEnter={({previousPosition, currentPosition, event}) => {
-                // do something useful!
-                loadMore(event);
-            }}
-            threshold={0}
-          />
           <a className="posts-load-more" onClick={loadMore}>Show More…</a>
       </div>
     )
