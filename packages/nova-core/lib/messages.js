1196040091fa5a20084d1e7d7a653c60086c300a
trujunzhang <trujunzhang@gmail.com>
2016-09-08 14:03:53 +0800

    message.

--- packages/nova-core/lib/messages.js
+++ packages/nova-core/lib/messages.js
@@ -1,24 +1,58 @@
+import React, {PropTypes, Component} from 'react';
+import PostDetailSet from "./postdetailset.js";
+import AppStatus from "./appstatus.js";
+import PopoverMenus from "./popovermenus.js"
+
 const Messages = {
-  // Local (client-only) collection
-  collection: new Meteor.Collection(null),
+    // Local (client-only) collection
+    collection: new Meteor.Collection(null),
 
-  flash(content, type) {
-    type = (typeof type === 'undefined') ? 'error': type;
-    // Store errors in the local collection
-    this.collection.insert({content:content, type:type, seen: false, show:true});
-  },
+    flash(content, type) {
+        type = (typeof type === 'undefined') ? 'error' : type;
+        // Store errors in the local collection
+        this.collection.insert({content: content, type: type, seen: false, show: true});
+    },
 
-  markAsSeen(messageId) {
-    this.collection.update(messageId, {$set: {seen:true}});
-  },
+    appStatus: new AppStatus(),
+    postDetailSet: new PostDetailSet(),
 
-  clear(messageId) {
-    this.collection.update(messageId, {$set: {show:false}});
-  },
+    layout: Component,
 
-  clearSeen() {
-    this.collection.update({seen:true}, {$set: {show:false}}, {multi:true});
-  }
+    registerCompont(layout){
+        this.layout = layout;
+        this.appStatus.registerLayout(layout);
+    },
+
+    pushAndPostShow(postId){
+        var cachePost = this.postDetailSet.push(postId);
+        this.appStatus.updateCachePost(cachePost);
+    },
+
+    dismissPostPanel(){
+        var cachePost = this.postDetailSet.lastPage();
+        this.appStatus.updateCachePost(cachePost);
+    },
+
+    showPopoverMenu(top, left, width, height, type){
+        var popoverMenus = new PopoverMenus(top, left, width, height, type);
+        this.layout.setState({popoverMenu: popoverMenus});
+    },
+
+    dismissPopoverMenu(){
+        this.layout.setState({popoverMenu: null});
+    },
+
+    markAsSeen(messageId) {
+        this.collection.update(messageId, {$set: {seen: true}});
+    },
+
+    clear(messageId) {
+        this.collection.update(messageId, {$set: {show: false}});
+    },
+
+    clearSeen() {
+        this.collection.update({seen: true}, {$set: {show: false}}, {multi: true});
+    }
 };
 
 export default Messages;
