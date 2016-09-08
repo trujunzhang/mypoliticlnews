5d1058354ac728e97d2d784dc69b950504ffc1f4
trujunzhang <trujunzhang@gmail.com>
2016-09-08 15:31:09 +0800

    message.

--- packages/nova-base-components/lib/common/Layout.jsx
+++ packages/nova-base-components/lib/common/Layout.jsx
@@ -1,37 +1,84 @@
 import Telescope from 'meteor/nova:lib';
-import React, { PropTypes, Component } from 'react';
-import { FlashContainer } from "meteor/nova:core";
+import React, {PropTypes, Component} from 'react';
+import {FlashContainer} from "meteor/nova:core";
+import {DocumentContainer} from "meteor/utilities:react-list-container";
+import Posts from "meteor/nova:posts";
 
 class Layout extends Component {
 
-  render() {
+    constructor(props) {
+        super(props);
+    }
 
-    return (
-      <div className="wrapper" id="wrapper">
+    componentWillMount() {
+        this.context.messages.registerCompont(this);
+        this.state = this.initialState = {
+            isSearching: false,
+            isLogin: false,
+            popoverMenu: null,
+            cachePost: null,
+            didMount: false,
+        };
+    }
 
-        <Telescope.components.HeadTags />
+    componentDidMount() {
+        this.setState({didMount: true});
+    }
 
-        <Telescope.components.UsersProfileCheck {...this.props} />
+    dismissCurrentPostPanel() {
+        this.context.messages.dismissPostPanel();
+    }
 
-        <Telescope.components.Header {...this.props}/>
-      
-        <div className="main">
+    showCurrentPostPanel(postId) {
+        this.context.messages.pushAndPostShow(postId);
+    }
 
-          <FlashContainer component={Telescope.components.FlashMessages}/>
+    render() {
+        let classValue = "wrapper" + (this.state.isSearching ? " search-mode" : "") + (this.state.cachePost ? " no-scroll" : "");
 
-          <Telescope.components.Newsletter />
+        return (
+          <div className={classValue} id="wrapper">
 
-          {this.props.children}
+              <Telescope.components.HeadTags />
 
-        </div>
-      
-        <Telescope.components.Footer {...this.props}/>
-      
-      </div>
-    )
+              <Telescope.components.UsersProfileCheck {...this.props} />
 
-  }
+              <div>
+                  <Telescope.components.Header {...this.props} />
+              </div>
+
+              {/*Rendering the popover menus*/}
+              {/*{this.renderPopoverMenus()}*/}
+
+              <div className={this.state.isSearching ? 'overlayActive_oQWJ3' : 'overlayInactive_1UI7W'}></div>
+              {/*Showing the post's detail pages*/}
+              {/*{this.renderPosts()}*/}
+              {/*Showing the login UI*/}
+              {/*{this.state.isLogin ? <Telescope.components.UserLogin /> : null}*/}
+
+              <div >
+                  <div className="constraintWidth_ZyYbM container_3aBgK">
+                      <FlashContainer component={Telescope.components.FlashMessages}/>
+                  </div>
+
+                  <Telescope.components.Newsletter />
+                  <div className="constraintWidth_ZyYbM container_3aBgK">
+                      {this.props.children}
+                  </div>
+
+              </div>
+
+              <Telescope.components.Footer {...this.props}/>
+
+          </div>
+        )
+
+    }
 }
+
+Layout.contextTypes = {
+    messages: React.PropTypes.object
+};
 
 Layout.displayName = "Layout";
 
